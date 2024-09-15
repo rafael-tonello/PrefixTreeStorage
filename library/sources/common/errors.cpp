a653691eed9aed19f82b46297a98034624aabb58
Rafael Tonello <tonello.rafinha@gmail.com>
2024-09-14 21:43:06 -0300

    GitHub action for deploy

--- library/sources/common/errors.cpp
+++
@@ -1,5 +0,0 @@
-#include  "errors.h" 
- 
-Error Errors::NoError = "";
-Error Errors::TimeoutError = "Timeout reached";
-Error Errors::ConnectionIsNotStablishedError = "The connection is not stablished";
